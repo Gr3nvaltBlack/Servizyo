0bdd0ec578269ec37596c2868ff7d75add548526
Gr3nvaltBlack <dlk9023x@gmail.com>
2026-05-08 01:48:46 +0200

    Creation and initialization of button components

---
+++ FRONT-END/src/style/styledelement.tsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+const ButtonStyled = styled.button `
+    cursor: pointer;
+    border-radius: 5px;
+    border: none;
+    padding: 1em 1.5em;
+    font-weight: 550;
+    transition: border-color 0.25s;
+`;
+export { ButtonStyled };
