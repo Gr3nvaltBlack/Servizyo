cad3edf8d774a5b370561058ec0adbad9c9bbd36
Gr3nvaltBlack <dlk9023x@gmail.com>
2026-05-11 01:13:53 +0200

    Added footer tag and navigation to the services page

--- FRONT-END/src/router/AppRouter.tsx
+++ FRONT-END/src/router/AppRouter.tsx
@@ -1,6 +1,8 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "../pages/HomePage";
+import Services from "../pages/Services";
 import NavBar from "../components/Navbar";
+import Footer from "../components/Footer";
 
 const AppRouter = () => {
     return (
@@ -8,7 +10,9 @@ const AppRouter = () => {
             <NavBar/>
             <Routes>
                 <Route path="/" element={<HomePage/>} index/>
+                <Route path="/services" element={<Services/>} />
             </Routes>
+            <Footer/>
         </Router>
     );
 };
