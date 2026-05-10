16a531465a19e65349e9e2b9b0944ac89e9a6890
Gr3nvaltBlack <dlk9023x@gmail.com>
2026-05-10 03:11:04 +0200

    Creating the navigation bar

---
+++ FRONT-END/src/components/Navbar.tsx
@@ -0,0 +1,40 @@
+import "../style/components.css"
+import { NavLink } from "react-router-dom";
+import { ButtonStyled } from "../style/styledelement";
+
+
+const NavBar = () => {
+    return (
+        <nav id="navigation-container">
+
+            <div id="navigation-information">
+                <img src="../../public/Servizyo_2.png" alt="Servizyo-icon" className="servizyo-icon"/>
+
+                    <ul className="navigation-theme">
+                        <li>
+                            <NavLink to={"/"}>Accueil</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to={""}>Services</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to={""}>Nos Prestataires</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to={""}>Comment ça marche</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to={""}>Contact</NavLink>
+                        </li>
+                    </ul>
+            </div>
+
+            <div className="navigation-button">
+                <ButtonStyled>Je suis prestataire</ButtonStyled>
+                <ButtonStyled>Trouver une aide</ButtonStyled>
+            </div>
+        </nav>
+    );
+}
+
+export default NavBar;
